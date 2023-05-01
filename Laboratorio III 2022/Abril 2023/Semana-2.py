25703ee291f4287dcb7ec9453ca5643e0d298af8
Flor Pons Toloza <111473222+FlorPons@users.noreply.github.com>
2023-05-01 15:41:12 -0300

    Florencia Pons/Creamos un archivo txt e info

---
+++ Laboratorio III 2022/Abril 2023/Semana-2.py
@@ -0,0 +1,13 @@
+#Florencia Pons
+#Creamos un archivo txt e incorporamos información 
+
+
+try:
+    archivo = open('prueba.txt', 'w') # La w es de write que significa escribir
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Con esto terminamos')
+except Exception as e:
+    print(e)
+finally: #Siempre se ejecuta
+    archivo.close() #Con esto se debe cerrar el archivo
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
