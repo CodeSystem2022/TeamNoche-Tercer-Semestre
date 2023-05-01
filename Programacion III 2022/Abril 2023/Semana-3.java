45844d29bcd7b3d5e1d64a02eba99ad98e9c30d0
Flor Pons Toloza <111473222+FlorPons@users.noreply.github.com>
2023-05-01 17:39:27 -0300

    Ciclo ForEach

--- Programacion III 2022/Abril 2023/Semana-3.java
+++ Programacion III 2022/Abril 2023/Semana-3.java
@@ -1,1 +1,17 @@
+/*Florencia Pons
+Ciclo ForEach*/
 
+
+package test;
+
+public class testForEach {
+    public static void main(String[] args) {
+        int edades[] = {5, 6, 8, 9}; //Sintaxis resumida
+        for (int edad: edades) { //Sintaxis del ForEach
+            System.out.println("edad = "+edad);
+            
+        }
+    }
+    
+}
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
