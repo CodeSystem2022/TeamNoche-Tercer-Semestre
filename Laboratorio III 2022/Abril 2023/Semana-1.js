26cf86f19d2b3ee938ecc754ecf56577c8ece9cf
Flor Pons Toloza <111473222+FlorPons@users.noreply.github.com>
2023-05-01 14:56:33 -0300

    Florencia Pons/Ciclo while y do while

--- Laboratorio III 2022/Abril 2023/Semana-1.js
+++ Laboratorio III 2022/Abril 2023/Semana-1.js
@@ -1,1 +1,19 @@
+//Florencia Pons 
+
+//Ciclo While
+let contador = 0;
+while(contador < 3){
+    console.log(contador);
+    contador++;
+}
+console.log("Fin del ciclo while");
+
+//do while
+let conteo = 0;
+do{
+    console.log(conteo);
+    conteo++;
+}while(conteo <3);
+console.log("Fin del ciclo do while");
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
