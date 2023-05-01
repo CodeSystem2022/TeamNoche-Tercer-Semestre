abecb9f6b197058a81536ade0b1e1a2c588419d8
Flor Pons Toloza <111473222+FlorPons@users.noreply.github.com>
2023-05-01 14:46:19 -0300

    Florencia Pons/Creación de objetos en JavaScript

--- Laboratorio III 2022/Abril 2023/Semana-4.js
+++ Laboratorio III 2022/Abril 2023/Semana-4.js
@@ -1,1 +1,18 @@
+//Florencia Pons
+//Creación de objetos en JavaScript
+let x = 10; //variable de tipo primitiva
+console.log(x.length);
 
+//objeto
+let persona = {
+    nombre:'Carlos',
+    apellido: 'Gil',
+    email: 'cgil@gmail.com',
+    edad : 30
+}
+
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.email);
+console.log(persona.edad);
+console.log(persona);
