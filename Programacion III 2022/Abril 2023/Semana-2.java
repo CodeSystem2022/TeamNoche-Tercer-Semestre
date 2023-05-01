e9857824ae0f16c982ee69588aab00a31dfe92c5
Flor Pons Toloza <111473222+FlorPons@users.noreply.github.com>
2023-05-01 17:27:11 -0300

    Argumentos variables

--- Programacion III 2022/Abril 2023/Semana-2.java
+++ Programacion III 2022/Abril 2023/Semana-2.java
@@ -1,1 +1,27 @@
+/*Florencia Pons
+Argumentos variables*/
 
+
+package test;
+
+public class testArgumentosVariables {
+    public static void main(String[] args){
+        imprimirNumeros(3, 4, 5);
+        imprimirNumeros(1, 2);
+        variosParametros("Juan", "Perez", 7, 8, 9);
+        
+    }
+    
+    private static void variosParametros(String nombre, String apellido, int ...numeros){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+        imprimirNumeros (numeros);
+    
+    }
+    private static void imprimirNumeros(int ...numeros){
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Elementos: "+numeros[i]);
+        }
+    }
+}
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
