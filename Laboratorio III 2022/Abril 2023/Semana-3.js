df333eaa82be58d38145a3b4cc899466ef7b9c8b
Flor Pons Toloza <111473222+FlorPons@users.noreply.github.com>
2023-05-01 15:09:19 -0300

    Florecia Pons/Funciones y return

--- Laboratorio III 2022/Abril 2023/Semana-3.js
+++ Laboratorio III 2022/Abril 2023/Semana-3.js
@@ -1,1 +1,15 @@
+//Florencia Pons
+//Funciones y return
+miFuncion(8, 2); //Esto se lo conoce como hosting
 
+function miFuncion(a, b){
+    //console.log("Sumamos: "+ (a+ b));
+    return a + b;
+}
+
+//Llamando la funcion 
+miFuncion(5, 4);
+
+let resultado = miFuncion(6, 7);
+console.log(resultado);
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
