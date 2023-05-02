4f3e62614d1f401ec8599667e1c7695f5f271a16
Ezequiel Soria <100471408+kelo72@users.noreply.github.com>
2023-05-02 16:05:42 -0300

    Soria: Modificador de Acceso PROTECTED
    Carga de la clase1 donde creamos la clase PROTECTED en java, correspondiente a la tercer semana de Abril.

--- Programacion III 2022/Abril 2023/Semana-3.java
+++ Programacion III 2022/Abril 2023/Semana-3.java
@@ -15,3 +15,30 @@ public class testForEach {
     
 }
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/**
+Julio Ezequiel Soria
+Modificador de acceso PROTECTED
+ */
+package paquete1;
+
+public class Clase1 {
+    public String atributoPublico = "Valor atributo publico";
+    protected String atributoProtected = "Valor atributo protected";
+
+    public Clase1() {
+        System.out.println("Constructor publico");
+    }
+    
+    protected Clase1(String atributoPublico){
+        System.out.println("Constructor protected");
+    }
+    
+    public void metodoPublico(){
+        System.out.println("Metodo publico");
+    }
+    
+    protected void metodoProtected(){
+        System.out.println("Metodo protected");
+    }
+}
+----------------------------------------------------------------------------------------------------------------------------------------------------------
