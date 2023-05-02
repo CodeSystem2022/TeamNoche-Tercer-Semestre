2cb19895178c206b0510043f2d9d5651ca926c3b
Ezequiel Soria <100471408+kelo72@users.noreply.github.com>
2023-05-02 17:20:21 -0300

    Soria: Paso por Referencia
    Clase de funciones es JS. Paso por referencia correspondiente a la clase de la tercera semana de JS.

--- Laboratorio III 2022/Abril 2023/Semana-3.js
+++ Laboratorio III 2022/Abril 2023/Semana-3.js
@@ -13,3 +13,21 @@ miFuncion(5, 4);
 let resultado = miFuncion(6, 7);
 console.log(resultado);
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Julio Ezequiel Soria
+//Paso por referencia
+const persona = {
+    nombre: 'juan',
+    apellido: 'Lepez'
+}
+console.log(typeof persona)
+
+function cambiarValorObjeto(p1) {//la variable p1 reasigna los datos a los parametros de nombre y apellido, luego es eliminada.
+    console.log(persona)
+    p1.nombre = 'Eze'
+    p1.apellido = 'Soria'
+    console.log(persona)
+}
+
+cambiarValorObjeto(persona)
+console.log(persona)
