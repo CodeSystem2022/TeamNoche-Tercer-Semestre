ee1420ea34d269def307957306295aa2469cd4e2
Ezequiel Soria <100471408+kelo72@users.noreply.github.com>
2023-05-02 17:07:29 -0300

    Soria: Palabra reservada BREAK
    Carga de ejerciocio de ciclos for con uso de la palabra BREAK. Correspondiente a la primera semana de JS.

--- Laboratorio III 2022/Abril 2023/Semana-1.js
+++ Laboratorio III 2022/Abril 2023/Semana-1.js
@@ -16,4 +16,13 @@ do{
 }while(conteo <3);
 console.log("Fin del ciclo do while");
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+//Julio Ezequiel Soria
+////Palabra reservada break
+for(i=0; i <= 10; i++){
+    if (i % 2 == 0) {
+        console.log(i)
+        break //termina al encontrar el primer numero par       
+    }
+}
+console.log('Termina el ciclo al encontrar el primer par')
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
