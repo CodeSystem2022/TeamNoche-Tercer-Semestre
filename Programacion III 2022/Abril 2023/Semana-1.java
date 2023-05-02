01d11e9aa8c46e0091fa7e849a92ee337d414c9c
Ezequiel Soria <100471408+kelo72@users.noreply.github.com>
2023-05-02 15:52:02 -0300

    Soria: creacion mundo pc
    Carga del ejercio de creacion de MundoPc en java, correspondiente a la primera semana de Abril.

--- Programacion III 2022/Abril 2023/Semana-1.java
+++ Programacion III 2022/Abril 2023/Semana-1.java
@@ -35,3 +35,58 @@ public class DispositivoEntrada {
     
 }
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/**
+Julio Ezequiel Soria
+Creacion de clase main de Mundo Pc
+ */
+package mundopc;
+
+import ar.com.system2023.mundopc.*;
+
+
+public class MundoPc {
+    public static void main(String[] args) {
+        
+        Monitor monitorHP = new Monitor("HP", 13);
+        Teclado tecladoHP = new Teclado("Bluetooth", "HP");
+        Raton ratonHP = new Raton("Bluetooth", "HP");
+        Computadora computadoraHP = new Computadora("Computadora HP", monitorHP, tecladoHP, ratonHP);
+        
+        Monitor monitorAcer = new Monitor("Acer", 18);
+        Teclado tecladoAcer = new Teclado("Bluetooth", "Acer");
+        Raton ratonAcer = new Raton("Bluetooth", "Acer");
+        Computadora computadoraAcer = new Computadora("Computadora Acer", monitorAcer, tecladoAcer, ratonAcer);
+        
+        Monitor monitorSamsung = new Monitor("Samsung", 19);
+        Teclado tecladoSamsung = new Teclado("Bluetooth", "Samsung");
+        Raton ratonSamsung = new Raton("Bluetooth", "Samsung");
+        Computadora computadoraSamsung = new Computadora("Computadora Samsung", monitorSamsung, tecladoSamsung, ratonSamsung);
+        
+        Monitor monitorGamer = new Monitor("Gamer", 24);
+        Teclado tecladoGamer = new Teclado("Bluetooth", "Gamer");
+        Raton ratonGamer = new Raton("Bluetooth", "Gamer");
+        Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
+        
+        Orden orden1 = new Orden();
+        orden1.agregarComputadora(computadoraHP);
+        orden1.agregarComputadora(computadoraAcer);
+        orden1.agregarComputadora(computadoraAcer);
+        orden1.agregarComputadora(computadoraAcer);
+        orden1.agregarComputadora(computadoraAcer);
+        orden1.agregarComputadora(computadoraAcer);
+        orden1.agregarComputadora(computadoraAcer);
+        orden1.agregarComputadora(computadoraAcer);
+        orden1.agregarComputadora(computadoraAcer);
+        orden1.agregarComputadora(computadoraAcer);
+        orden1.agregarComputadora(computadoraAcer);
+       
+        orden1.mostrarOrden();
+        
+        Computadora computadoraArmada = new Computadora("PC Armada", monitorSamsung, tecladoGamer, ratonGamer);
+        Orden orden2 = new Orden();
+        orden2.agregarComputadora(computadoraArmada);
+        
+        orden2.mostrarOrden();
+     }
+    
+}
