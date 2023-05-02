a98ff10b61212e25a865170b8cff0655a3ce605a
Ezequiel Soria <100471408+kelo72@users.noreply.github.com>
2023-05-02 15:58:33 -0300

    Soria: Bloques inicializadores.
    Carga de la tarea de bloques de inicializacion correspondiente a la segunda semana de Abril.

--- Programacion III 2022/Abril 2023/Semana-2.java
+++ Programacion III 2022/Abril 2023/Semana-2.java
@@ -25,3 +25,41 @@ public class testArgumentosVariables {
     }
 }
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/**
+ Julio Ezequiel Soria
+ Creación de bloques de inicialización, estaticos y no estaticos
+ */
+
+    package domain;
+
+public class Persona {
+    private final int idPersona;
+    private static int contadorPersonas;
+    
+    static{ //Bloque de inicializacion estatico, solo se puede utilizar atributos estaticos.
+        System.out.println("Inicia el bloque STATIC");
+        ++Persona.contadorPersonas;
+    }
+    
+    { //Bloque de inicializacion no estatico (Contexto dinamico)
+        System.out.println("Ejecucion del bloque No estatico");
+        this.idPersona = Persona.contadorPersonas++; //Incrementamos el atributo
+    }
+    //Los bloeque de inicializacion se ejecutan antes del constructor
+
+    public Persona() {
+        System.out.println("Esta es la ejecución del constructor");
+    }
+
+    public int getIdPersona() {
+        return this.idPersona;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + '}';
+    }
+    
+    
+}
+----------------------------------------------------------------------------------------------------------------------------------------------------------
