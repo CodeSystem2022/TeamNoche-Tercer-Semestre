82fa959e1a95ca81c237a856fc8328634c52e4e2
Ezequiel Soria <100471408+kelo72@users.noreply.github.com>
2023-05-04 23:28:28 -0300

    Soria: Escritura de archivo txt
    Ejecucion de escritura en un txt en python, Clase que corresponde a la tercer seman de Abril.

--- Laboratorio III 2022/Abril 2023/Semana-2.py
+++ Laboratorio III 2022/Abril 2023/Semana-2.py
@@ -11,3 +11,17 @@ except Exception as e:
 finally: #Siempre se ejecuta
     archivo.close() #Con esto se debe cerrar el archivo
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Soria Julio Ezequiel
+# Anexamos informacion y copiamos a otro txt.
+archivo2 = open('copia.txt', 'a', encoding= 'utf8') # con la letra A cada vez que se ejecute 
+                                                    #el comando se agregara una nueva copia debajo de la anterior.
+
+archivo2 = open('copia.txt', 'w', encoding= 'utf8') # con la letra W no importa cuantas veces se ejecute 
+                                                    #el comando solo se copiara una vez
+
+archivo2.write(archivo.read()) # copiamos el archivo 1 en el 2
+archivo.close() # Cerramos priomero el archivo a copiar
+archivo2.close() # Cerramos la copia
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
