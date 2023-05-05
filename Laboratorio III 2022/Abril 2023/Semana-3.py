78a1de2c5c203479fda5890d396fcbf9e4f7a867
Ezequiel Soria <100471408+kelo72@users.noreply.github.com>
2023-05-04 23:32:21 -0300

    Soria: Clase catalogo
    Creacion del catalogo de peliculas en python, correspondiente a la cuarta semana de Abril.

--- Laboratorio III 2022/Abril 2023/Semana-3.py
+++ Laboratorio III 2022/Abril 2023/Semana-3.py
@@ -16,3 +16,27 @@ class Pelicula:
     def nombre(self, nombre):
         self._nombre = nombre
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Soria Julio Ezequiel
+#Creacion de la clase CatalogoPeliculas
+import os
+
+class CatalogoPeliculas:
+
+    ruta_archivo = 'c:\\Users\ezequiel\Documents\GitHub\Tecnicatura3semestre\Laboratorio3\Python\Leccion03\catalogo_peliculas\peliculas.txt'
+    @classmethod
+    def agregar_pelicula(cls, nombre, anio, categoria):
+        with open(cls.ruta_archivo, 'a', encoding= 'utf8') as archivo:
+            archivo.write(f'Titulo: {nombre} / Año: {anio} / Categoria: {categoria} \n')
+
+    @classmethod
+    def listar_peliculas(cls):
+        with open(cls.ruta_archivo, 'r', encoding= 'utf8') as archivo:
+            print(f'Catalogo de peliculas'.center(50, '-'))
+            print(archivo.read())
+
+    @classmethod
+    def eliminar_peliculas(cls):
+        os.remove(cls.ruta_archivo)
+        print(f'Archivo eliminado: {cls.ruta_archivo}')
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
