767670e189b540c66b0a9ec635696f99f264386d
Flor Pons Toloza <111473222+FlorPons@users.noreply.github.com>
2023-05-07 22:14:04 -0300

    Trabajamos en PostgreSQL
    Creamos una base de datos llamada test_bd y creamos la tabla "Persona" dentro de ella.
    Realizamos consultas e incorporamos elementos en la tabla

--- Laboratorio III 2022/Abril 2023/Clase-4.py
+++ Laboratorio III 2022/Abril 2023/Clase-4.py
@@ -1,1 +1,15 @@
+#Florencia Pons
+#POSTGRE SQL
+#Creamos una base de datos llamada test_bd y creamos la tabla "Persona" dentro de ella
+SELECT * FROM public."Persona"
+ORDER BY id_persona ASC 
 
+#Realizamos consultas e incorporamos elementos en la tabla
+--Consulta informacion seleccionada
+--SELECT * FROM "Persona" WHERE id_persona in (1,2,3) 
+--Ingresamos un nuevo elemento a la tabla
+--INSERT INTO "Persona"(nombre,apellido,email)VALUES ('Susana', 'Lara', 'slara@mail.com') 
+--Hacemos la consulta para ver toda la informacion de la tabla
+--SELECT * FROM "Persona"
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
