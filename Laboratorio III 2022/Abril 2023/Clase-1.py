acdd9013f63caaea4eefa3ec3a4bda76023dd23b
Carlos Hernan <86528810+Hernan-DOS@users.noreply.github.com>
2023-05-07 23:17:05 -0300

    update clase 1 python
    Agregue el ejercicio  #Bloques else y finally al manejar excepciones

--- Laboratorio III 2022/Abril 2023/Clase-1.py
+++ Laboratorio III 2022/Abril 2023/Clase-1.py
@@ -21,3 +21,27 @@ except TypeError as e:
     print(f'1) Ocurrio un error: {e}')
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#Suarez, Carlos Hernan
+#Bloques else y finally al manejar excepciones
+
+try:
+    a = int(input('Digite el primer numero: '))
+    b = int(input('Digite el segundo numero: '))
+    resultado = a / b # modificamos
+except TypeError as e:
+    print(f'TypeError - Ocurrió un error: {type(e)}')
+except ZeroDivisionError as e:
+    print(f'ZeroDivisionError - Ocurrió un error: {type(e)}')
+except Exception as e:
+    print(f'Exception - Ocurrió un error: {type(e)}')
+else :
+    print('No se arrojo ninguna excepcion')
+finally: #Siempre se va a ejecutar
+    print('Ejecucion de este bloque finally')
+    
+print(f'El resultado es: {resultado}')
+print('Seguimos...')
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
