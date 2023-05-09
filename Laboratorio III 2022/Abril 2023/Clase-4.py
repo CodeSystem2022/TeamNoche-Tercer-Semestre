77b28450a21f85486ae5d0d2fcc10cd33ad7dc12
Ezequiel Soria <100471408+kelo72@users.noreply.github.com>
2023-05-08 21:16:13 -0300

    Coneccion a db
    Coneccion de db de postgreSql y Python de la clase 4

--- Laboratorio III 2022/Abril 2023/Clase-4.py
+++ Laboratorio III 2022/Abril 2023/Clase-4.py
@@ -13,3 +13,25 @@ ORDER BY id_persona ASC
 --SELECT * FROM "Persona"
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Soria Ezequiel
+# Conexion de db en python y postgresql
+import psycopg2 # Importamos el paquete que permite la conexion a la BD
+
+# Es muy importante tener bien los datos tando de la base de dato como usuario e ip.
+conexion = psycopg2.connect(
+    database = 'test_bd',
+    user = 'postgres',
+    password = 'Admin',
+    host = '127.0.0.1',
+    port = '5432'
+)
+
+#Creamos el "cursor" que es el encargado de ejecutar las sentencias y las query entre el programa y la db. 
+cursor = conexion.cursor()
+sentencia = 'SELECT * FROM persona'
+cursor.execute(sentencia)
+registros = cursor.fetchall()
+
+print(registros)
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
