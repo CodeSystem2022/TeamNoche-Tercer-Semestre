78d3c4940baf4e15f9b26b4f8ae43d7f9af42eb6
Flor Pons Toloza <111473222+FlorPons@users.noreply.github.com>
2023-05-12 23:00:41 -0300

    Creamos la clase empleado

--- Programacion III 2022/Mayo 2023/Semana-6.java
+++ Programacion III 2022/Mayo 2023/Semana-6.java
@@ -24,6 +24,43 @@ public class Escritor extends Empleado{
         return "Escritor(" + "tipoEscritura=" + tipoEscritura + '}' + " "+super.toString();
     }
     
+   //Creamos la clase Empleado
+    
+package domain;
+
+public class Empleado {
+   protected String nombre;
+    protected double sueldo;
+    
+    public Empleado(String nombre, double sueldo){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+    
+    //Método para la sobreescritura
+    public String obtenerDetalles(){
+        return "Nombre: "+this.nombre+", Sueldo: "+this.sueldo;
+    }    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+    
+      
+}
+
     public TipoEscritura getTipoEscritura(){
         return this.tipoEscritura;
     }
