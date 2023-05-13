b4a833f9ffee362be0821f2778e83fd5af02759d
Flor Pons Toloza <111473222+FlorPons@users.noreply.github.com>
2023-05-12 23:27:56 -0300

    Proyecto Conversión de Objetos
    Creamos la clase Empleado

--- Programacion III 2022/Mayo 2023/Clase-5.java
+++ Programacion III 2022/Mayo 2023/Clase-5.java
@@ -1,1 +1,40 @@
+//Florecia Pons
+//Proyecto Conversión de Objetos
+//Creamos la clase Empleado
+
+package domain;
+
+public class Empleado {
+   protected String nombre;
+    protected double sueldo;
+    
+    public Empleado(String nombre, double sueldo){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+    
+    //Método para la sobreescritura
+    public String obtenerDetalles(){
+        return "Nombre: "+this.nombre+", Sueldo: "+this.sueldo;
+    }    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+    
+      
+}
+
 
