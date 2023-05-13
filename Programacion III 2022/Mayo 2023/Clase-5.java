40f14ad09a5fc15c141efb0a58d13fc4f5d7a61f
Ezequiel Soria <100471408+kelo72@users.noreply.github.com>
2023-05-13 10:13:40 -0300

    Carga del la clase test

--- Programacion III 2022/Mayo 2023/Clase-5.java
+++ Programacion III 2022/Mayo 2023/Clase-5.java
@@ -1,1 +1,28 @@
+//Soria Ezequiel
+   //Creamos la clase TEST
+package Test;
 
+/**
+ * @author ezequiel
+ */
+import domain.*;
+
+public class TestConversionObjetos {
+    public static void main(String[] args) {
+        Empleado empleado;
+        empleado = new Escritor("Juan", 5000, TipoEscritura.CLASICO);
+        //System.out.println("empleado = " + empleado);
+        //System.out.println(empleado.obtenerDetalles());
+        
+        //Downcasting
+        ((Escritor)empleado).getTipoEscritura(); //una forma de hacerlo
+        
+        Escritor escritor = (Escritor)empleado; //Segunda forma
+        escritor.getTipoEscritura();
+        
+        //Upcasting
+        Empleado empleado2 = escritor;
+        System.out.println(empleado2.obtenerDetalles());
+    }
+}
+----------------------------------------------------------------------------------
