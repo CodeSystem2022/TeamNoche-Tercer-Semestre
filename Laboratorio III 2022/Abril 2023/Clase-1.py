7452bfbdb531237330d61e1aa9dfec933fec8d90
Carlos Hernan <86528810+Hernan-DOS@users.noreply.github.com>
2023-05-15 18:14:28 -0300

    Update Clase-1.py
    Clase 1 en python completo

--- Laboratorio III 2022/Abril 2023/Clase-1.py
+++ Laboratorio III 2022/Abril 2023/Clase-1.py
@@ -24,7 +24,7 @@ except TypeError as e:
 
 
 #Suarez, Carlos Hernan
-#Bloques else y finally al manejar excepciones
+#1.5 Bloques else y finally al manejar excepciones
 
 try:
     a = int(input('Digite el primer numero: '))
@@ -76,5 +76,49 @@ else:
 finally:
     print('Ejecución de este bloque finally')
     
+print(f'El resultado es: {resultado}')
+print('Seguimos...')
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Suarez, Carlos Hernan
+#1.4 Más de procedimientos de excepciones
+
+resultado = None
+
+try:
+    a = int(input('Digite el primer numero: '))
+    b = int(input('Digite el segundo numero: '))
+    resultado = a / b  # Modificamos
+except TypeError as e:
+    print(f'TypeError - Ocurrió un error: {type(e)}')
+except ZeroDivisionError as e:
+    print(f'ZeroDivisionError - Ocurrió un error: {type(e)}')
+except Exception as e:
+    print(f'Exception - Ocurrió un error: {type(e)}')
+
+print(f'El resultado es: {resultado}')
+print('Seguimos...')
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Suarez, Carlos Hernan
+#1.3 Procesar clases de exception más específicas
+
+resultado = None
+a = 7
+b = 0
+
+try:
+    resultado = a / b  # modificamos
+except TypeError as e:
+    print(f'TypeError - Ocurrió un error: {type(e)}')
+except ZeroDivisionError as e:
+    print(f'ZeroDivisionError - Ocurrió un error: {type(e)}')
+except Exception as e:
+    print(f'Exception - Ocurrió un error: {type(e)}')
+
 print(f'El resultado es: {resultado}')
 print('Seguimos...')
