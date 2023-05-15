02e84ab76fc699220294033f51ba2ebff7b3b2f7
Carlos Hernan <86528810+Hernan-DOS@users.noreply.github.com>
2023-05-15 17:47:50 -0300

    Update Clase-1.js
    Clase 1  acomodada y quedo completa.

--- Laboratorio III 2022/Abril 2023/Clase-1.js
+++ Laboratorio III 2022/Abril 2023/Clase-1.js
@@ -1,5 +1,4 @@
 //Florencia Pons 
-
 //Ciclo While
 let contador = 0;
 while(contador < 3){
@@ -8,6 +7,10 @@ while(contador < 3){
 }
 console.log("Fin del ciclo while");
 
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Julio Ezequiel Soria
 //do while
 let conteo = 0;
 do{
@@ -15,8 +18,9 @@ do{
     conteo++;
 }while(conteo <3);
 console.log("Fin del ciclo do while");
+
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//Julio Ezequiel Soria
+//Carlos Hernan Suarez
 ////Palabra reservada break
 for(i=0; i <= 10; i++){
     if (i % 2 == 0) {
@@ -25,4 +29,49 @@ for(i=0; i <= 10; i++){
     }
 }
 console.log('Termina el ciclo al encontrar el primer par')
+
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Carlos Hernan Suarez
+//Usamos la palabra reservada Labels en javascript
+
+inicio:
+for(let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !== 0){
+        break inicio; //ir a la siguiente iteracion
+    }
+console.log(contando); 0
+}
+console.log("Termina el ciclor"); Termina el ciclo
+
+   
+    
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Carlos Hernan Suarez
+//Usamos la palabra reservada continue en Js    
+    
+for(let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !== 0){
+        continue; //ir a la siguiente iteraccion
+    }
+console.log(contando); 0, 2, 4, 6, 8, 10
+}
+console.log("Termina el ciclo"); Termina el ciclo    
+       
+    
+    
+ -----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Carlos Hernan Suarez
+// Creamos el ciclo for en Js    
+    
+for(let contando = 0: contando < 3; contando++){
+    console.log(contando); 0, 1, 2
+    }
+console.log("Fin del ciclo for"); Fin ciclo for
+    
+    
+    
+    
+
