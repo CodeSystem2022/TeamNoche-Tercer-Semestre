ec43232a4199e4f2384db3d3d9df58e777d040a1
Carlos Hernan <86528810+Hernan-DOS@users.noreply.github.com>
2023-05-15 18:10:30 -0300

    Update Clase-1.py

--- Laboratorio III 2022/Abril 2023/Clase-1.py
+++ Laboratorio III 2022/Abril 2023/Clase-1.py
@@ -45,3 +45,36 @@ print(f'El resultado es: {resultado}')
 print('Seguimos...')
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Suarez, Carlos Hernan
+#1.6 Creación de clases de Exception personalizadas
+
+class NumerosIgualesException(Exception):
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+        
+        
+from NumerosIgualesException import NumerosIgualesException
+
+try:
+    a = int(input('Digite el primer numero: '))
+    b = int(input('Digite el segundo numero: '))
+
+    if a == b:
+        raise NumerosIgualesException('Son numeros iguales')
+
+    resultado = a / b  # Modificamos
+
+except TypeError as e:
+    print(f'TypeError - Ocurrió un error: {type(e)}')
+except ZeroDivisionError as e:
+    print(f'ZeroDivisionError - Ocurrió un error: {type(e)}')
+except Exception as e:
+    print(f'Exception - Ocurrió un error: {type(e)}')
+else:
+    print('No se arrojó ninguna excepción')
+finally:
+    print('Ejecución de este bloque finally')
+    
+print(f'El resultado es: {resultado}')
+print('Seguimos...')
