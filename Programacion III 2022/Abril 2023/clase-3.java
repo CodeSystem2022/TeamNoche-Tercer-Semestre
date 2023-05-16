cfde71a0bd5a045dd359542e21f6e7e823722393
AlfredoMarino123 <112595990+AlfredoMarino123@users.noreply.github.com>
2023-05-15 22:38:32 -0300

    Update clase-3.java

--- Programacion III 2022/Abril 2023/clase-3.java
+++ Programacion III 2022/Abril 2023/clase-3.java
@@ -42,3 +42,21 @@ public class Clase1 {
     }
 }
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
+/*Marino afredo
+MODIFICADORES DE ACCESO PÚBLICO */
+
+package paquete1;
+
+public class Clase1 {
+    public String atributoPublic = "Valor atributo public";
+    
+    public Clase1(){
+        System.out.println("Constructor public");
+    }
+    
+ 
+    public void metodoPublico(){
+        System.out.println("Método públic");
+  
+    }
+}
