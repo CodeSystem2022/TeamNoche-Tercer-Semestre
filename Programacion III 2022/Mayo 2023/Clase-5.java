1a7c0eca1b288818fb41aa0c4384153447876e16
AlfredoMarino123 <112595990+AlfredoMarino123@users.noreply.github.com>
2023-05-23 09:13:55 -0300

    Update Clase-5.java

--- Programacion III 2022/Mayo 2023/Clase-5.java
+++ Programacion III 2022/Mayo 2023/Clase-5.java
@@ -115,3 +115,36 @@ public class TestConversionObjetos {
         System.out.println(empleado2.obtenerDetalles());
     }
 }
+-----------------------------------------------------------------------------
+   //5.4 Hashcode y Equals:
+// Marino Alfredo
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.nombre);
+        hash = 53 * hash + (int) (Double.doubleToLongBits(this.sueldo) ^ (Double.doubleToLongBits(this.sueldo) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Empleado other = (Empleado) obj;
+        if (Double.doubleToLongBits(this.sueldo) != Double.doubleToLongBits(other.sueldo)) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        return true;
+    }
+}
