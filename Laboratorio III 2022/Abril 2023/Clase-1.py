07e1135790ea43c7c36a3b1e1160839f334212e5
Yamila Contreras <112906065+Yami-Contreras@users.noreply.github.com>
2023-05-26 23:05:43 -0300

    Update Clase-1.py

--- Laboratorio III 2022/Abril 2023/Clase-1.py
+++ Laboratorio III 2022/Abril 2023/Clase-1.py
@@ -123,34 +123,5 @@ except Exception as e:
 print(f'El resultado es: {resultado}')
 print('Seguimos...')
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------
-#Contreras, Yamila Belen
-#Creación de clases Exception personalizadas
 
-from NumerosIgualesException import NumerosIgualesException
 
-resultado = None
-
-try:
-    a = int(input('Digite el primer número: '))
-    b = int(input('Digite el segundo número: '))
-    if a == b:
-        raise NumerosIgualesException('Son números iguales')
-    resultado = a / b # modificamos
-except TypeError as e:
-    print(f'TypeError - Ocurrió un error: {type(e)}')
-except ZeroDivisionError as e:
-    print(f'ZeroDivisionError - Ocurrió un error: {type(e)}')
-except Exception as e:
-    print(f'Exception - Ocurrió un error: {type(e)}')
-else:
-    print('No arrojó ninguna excepción')
-finally: # Siempre se va a ejecutar
-    print('Ejecución de este bloque finally')
-
-print(f'El resultado es: {resultado}')
-print('seguimos...')
-
-class NumerosIgualesException(Exception): #Extiende de la clase
-    def __instancecheck__(self, mensaje):
-        self.message = mensaje
