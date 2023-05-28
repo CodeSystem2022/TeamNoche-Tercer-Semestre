68ae1c48660801bf3e0fc71d49ab7dae0bc903d5
Yamila Contreras <112906065+Yami-Contreras@users.noreply.github.com>
2023-05-28 12:55:18 -0300

    Update Clase-3.py

--- Laboratorio III 2022/Abril 2023/Clase-3.py
+++ Laboratorio III 2022/Abril 2023/Clase-3.py
@@ -57,3 +57,32 @@ while opcion != 4:
         opcion = None
     else:
         print('Salimos del programa')
+        
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Contreras, Yamila Belen
+# Pruebas ingresando otras clases (Parte 1)
+
+from dominio.Pelicula import Pelicula
+from servicio.catalogo_peliculas import CatalogoPeliculas as cp
+opcion = None
+while opcion != 4:
+    try:
+        print('Opciones: ')
+        print('1. Agregrar Película')
+        print('2. Lista de Películas')
+        print('3. Eliminar Catálogo de Películas')
+        print('4. Salir')
+        opcion = int(input('Digite una opción de menú (1-4): '))
+        if opcion == 1:
+            nombre_pelicula = input('Digite el nombre de la película: ')
+            pelicula = Pelicula(nombre_pelicula)
+            cp.agregar_peliculas(pelicula)
+        elif opcion == 2:
+            cp.listar_peliculas()
+        elif == 3:
+            cp.eliminar_peliculas()
+    except Exception as e:
+        print(f'Ocurrió un error de tipo: {e}')
+        opcion = None
+    else:
+        print('Salida del programa')
