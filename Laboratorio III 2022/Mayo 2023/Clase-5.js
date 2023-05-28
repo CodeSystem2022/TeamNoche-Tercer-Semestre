35c8807a9ef14ec51bbf3d7bd0d568c85e16b134
Franco J. Videla <francojvidela@gmail.com>
2023-05-28 19:41:24 -0300

    Agregar metodos al constructor del objeto

--- Laboratorio III 2022/Mayo 2023/Clase-5.js
+++ Laboratorio III 2022/Mayo 2023/Clase-5.js
@@ -1,1 +1,23 @@
+// Franco J. Videla
+// Agregar metodos al constructor del objeto
+
+function Persona3(nombre, apellido, email){ //contructor
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre+' '+this.apellido;
+    }
+}
+let padre = new Persona3('Franco','Videla','francojvidela@gmail.com');
+padre.nombre = 'Juan Carlos';
+padre.telefono = '5492604372581'; // Una propiedad exclusiva del objeto padre
+console.log(padre);
+console.log(padre.nombreCompleto()); // Utilizamos la funcion
+let madre = new Persona3('Francisca','Gimenez','gimenezfrancisca@gmail.com');
+console.log(madre);
+console.log(madre.telefono); // La propiedad no esta definida
+console.log(madre.nombreCompleto());
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
 
