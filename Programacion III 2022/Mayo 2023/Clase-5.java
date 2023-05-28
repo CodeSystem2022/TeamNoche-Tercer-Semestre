123c53e82920cdff479b3d5a85f9c6fb0df5f799
Bruno Marchetti <107437629+Br1marchetti@users.noreply.github.com>
2023-05-28 18:17:12 -0300

    Update Clase-5.java

--- Programacion III 2022/Mayo 2023/Clase-5.java
+++ Programacion III 2022/Mayo 2023/Clase-5.java
@@ -182,3 +182,52 @@ public class TestClaseObject {
         }
     }
 }
+-----------------------------------------------------------------------------
+   //5.5 Clases Abstractas (abstract)
+   //Bruno Marchetti
+   //Figura Geometrica
+
+package domain;
+
+public abstract class FiguraGeometrica {
+    protected String tipoFigura;
+
+    protected FiguraGeometrica(String tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
+    public abstract void dibujar();
+
+    public String getTipoFigura() {
+        return tipoFigura;
+    }
+
+    public void setTipoFigura(String tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
+    @Override
+    public String toString() {
+        return "FiguraGeometrica{" +
+                "tipoFigura='" + tipoFigura + '\'' +
+                '}';
+    }
+}
+
+-----------------------------------------------------------------------------
+   //5.5 Clases Abstractas (abstract)
+   //Bruno Marchetti
+   //Figura Rectangulo
+   
+   package domain;
+
+public class Rectangulo extends FiguraGeometrica{
+    public Rectangulo(String tipoFigura) {
+        super(tipoFigura);
+    }
+
+    @Override
+    public void dibujar() {
+        System.out.println("Se imprime un: " + this.getClass().getSimpleName());
+    }
+}
