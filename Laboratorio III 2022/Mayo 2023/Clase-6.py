b9b0f33c98434c36724b2b2fdebd12bd2646cc5f
MSoriano <marlenesoriano1209@gmail.com>
2023-05-28 21:09:37 -0300

    Transacciones

--- Laboratorio III 2022/Mayo 2023/Clase-6.py
+++ Laboratorio III 2022/Mayo 2023/Clase-6.py
@@ -24,3 +24,25 @@ finally:
 
 #--------------------------------------------------------------------------------------------------------------------------------------------
 
+# Marlene Soriano
+# Transacciones
+
+import psycopg2 as bd # Esto es para poder conectarnos a Postgre
+
+conexion = bd.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    cursor = conexion.cursor()
+    sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+    valores = ('Marlene', 'Soriano', 'marlenesoriano1209@gmail.com')
+    cursor.execute(sentencia, valores)
+    conexion.commit()  # Realizamos el commit manualmente
+    print('Termina la transacción')
+except Exception as e:
+    conexion.rollback()
+    print(f'Ocurrió un error, se hizo un rollback: {e}')
+finally:
+    conexion.close()
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+
+#
