52414c0a9c06aa9c1d0c16123c45cceae429a224
Bruno Marchetti <107437629+Br1marchetti@users.noreply.github.com>
2023-05-31 19:12:50 -0300

    Update Clase-5.py

--- Laboratorio III 2022/Mayo 2023/Clase-5.py
+++ Laboratorio III 2022/Mayo 2023/Clase-5.py
@@ -198,8 +198,9 @@ except Exception as e:
     print(f'Ocurrió un error: {e}')
 finally:
     conexion.close(
-        
-     #Función fetchone en psycopg2 
+     
+#Bruno Marchetti        
+     #5.2Función fetchone en psycopg2 
         import psycopg2  
 
 conexion = psycopg2.connect(
@@ -212,6 +213,30 @@ conexion = psycopg2.connect(
 try:
     with conexion:
         with conexion.cursor() as cursor:
+   
+    #Bruno Marchetti    
+    #5.3 Función fechall en psycopg2 Parte 1 y 2    
+        import psycopg2  
+
+conexion = psycopg2.connect(
+    user="postgres",
+    password="Admin",
+    host="127.0.0.1",
+    port="5432",
+    database="test_bd"
+)
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = "SELECT * FROM persona WHERE id_persona = %s"  # Plceholder
+            id_persona = input("Digite un numero para el id persona: ")
+            cursor.execute(sentencia, (id_persona,))  # De esta manera ejecutamos la sentencia
+            registros = cursor.fetchone()  # Recuperamos todos los registros  que seran una lista
+            print(registros)
+except Exception as e:
+    print(f"Ocurrio un error: {e}")
+finally:
+    conexion.close()
             sentencia = "SELECT * FROM persona WHERE id_persona IN %s"  # Plceholder
             entrada = input("Digite los id_persona a buscar (separados por coma): ")
             llaves_primarias = (tuple(entrada.split(", ")),)
