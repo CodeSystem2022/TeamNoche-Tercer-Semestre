91dded1d70f09bad852e39d46d9aed358c1f27eb
Alfredo Marino <112595990+AlfredoMarino123@users.noreply.github.com>
2023-06-02 10:51:49 -0300

    Update Clase-4.js

--- Laboratorio III 2022/Abril 2023/Clase-4.js
+++ Laboratorio III 2022/Abril 2023/Clase-4.js
@@ -52,3 +52,18 @@ let personaString = JSON.stringify(persona); // convierte el objeto en una caden
 console.log(personaString);
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Marino Alfredo.
+//Como acceder a propiedades de los objetos 
+
+let persona2 = new Object();//Debe crear un nuevo objeto en memoria 
+persona2.nombre = 'Juan';
+persona2.direccion = 'Salada 14';
+persona2.telefono = '5492618282821';
+console.log(persona2.telefono); '5492618282821';
+
+console.log(persona['apellido']);// accedemos como si fuera un arreglo de Gil
+//for in
+for(propidad in persona){
+    console.log(propidad); nombre, apellido, email, edad, nombreCompleto
+    console.log(persona[propidad]); Carlos, Gil, cgil@gmail.com, 30, 
+}
