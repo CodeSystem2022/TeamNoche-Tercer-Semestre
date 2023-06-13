19a31051177dcaaddcf98afe9490c26c05c12375
Carlos Hernan <86528810+Hernan-DOS@users.noreply.github.com>
2023-06-12 23:52:06 -0300

    Update Clase-5.py

--- Laboratorio III 2022/Mayo 2023/Clase-5.py
+++ Laboratorio III 2022/Mayo 2023/Clase-5.py
@@ -1,3 +1,32 @@
+## carlos hernan suarez
+## 5.4 Insertar un registro con psycopg2
+import psycopg2
+
+conexion = psycopg2.connect(
+        user='postgres', password='matiitas', host='127.0.0.1', port='5432', database='test_bd',
+    )
+try:
+        with conexion:
+            with conexion.cursor() as cursor:
+                    sentencia = 'INSERT INTO persona (nombre, apellido, email) VALUES (%s, %s, %s)'
+                    valores=(
+                        ('Carlos','Lara','Clara@mail.com'),
+                        ('Marcos','Canto','mcanto@mail.com'),
+                        ('Marcelo','Cuenca','cuencaM@mail.com')
+                    )#tupla de tuplas
+                    cursor.executemany(sentencia,valores)  # de esta manera ejecutamos la sentencia
+                    #conexion.commit() guarda los cambios en la base de datos pero el with lo hace
+                    registros_insertados = cursor.rowcount
+                    print(f'Los registros insertados son: {registros_insertados}')
+
+
+except Exception as e:
+        print(f'Ocurrio un error:{e}')
+finally:
+        conexion.close()
+#-------------------------------------------------------------------------------------------------------------------------------------------
+
+
 # Marlene Soriano
 # Uso de with y funcion fetchone en psycopg2
 
