ca8f61b9d89cff410329f1bb000d3b6015bf65ee
Franco J. Videla <francojvidela@gmail.com>
2023-06-14 17:13:40 -0300

    update clase8.py

--- Laboratorio III 2022/Mayo 2023/Clase8.py
+++ Laboratorio III 2022/Mayo 2023/Clase8.py
@@ -1,1 +1,6 @@
-
+class Persona:
+    def __init__(self, id_persona, nombre, apellido, email):
+        self._id_persona = id_persona
+        self._nombre = nombre
+        self._apellido = apellido
+        self._email = email
