57f5886e43b8add63f8448e121d3438702f8bf0d
Franco J. Videla <francojvidela@gmail.com>
2023-06-14 17:20:14 -0300

    update clase8.py

--- Laboratorio III 2022/Mayo 2023/Clase8.py
+++ Laboratorio III 2022/Mayo 2023/Clase8.py
@@ -7,3 +7,14 @@ class Persona:
         self._nombre = nombre
         self._apellido = apellido
         self._email = email
+
+    def __str__(self):
+        return f'Id Persona: {self._id_persona},' \
+               f'Nombre: {self._nombre},' \
+               f'Apellido: {self._apellido},' \
+               f'Email: {self._email}'
+
+        # Metodos getters and setters
+        @property
+        def id_persona(self):
+            return self._id_persona
