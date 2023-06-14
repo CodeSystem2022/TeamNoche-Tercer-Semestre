0557caf21e74de161be8da13e0652082c828c6a3
Franco J. Videla <francojvidela@gmail.com>
2023-06-14 17:48:29 -0300

    update clase8.4.py

--- Laboratorio III 2022/Mayo 2023/Clase8.py
+++ Laboratorio III 2022/Mayo 2023/Clase8.py
@@ -46,3 +46,80 @@ class Persona:
         @email.setter
         def email(self,email):
             self._email = email
+
+if __name__ == '__main__':
+    persona1 = Persona(1, 'Franco', 'Videla', 'francojvidela@mail.com')
+    log.debug(persona1)
+    persona2 = Persona(nombre='Marlene', apellido='Soriano', email='marlenesoriano1209@mail.com')
+    log.debug(persona2)
+    persona1 = Persona(id_persona = 1)
+    log.debug(persona1)
+
+#por continuar
+
+#---------------------------------------------------------------------------------------------------------------------------
+
+# Franco J. Videla
+# 8.4 Comienzo de la creacion de la Clase PersonaDAO
+
+from capa_datos_persona.Persona import Persona
+from capa_datos_persona.conexion import Conexion
+from logger_base import log
+
+
+class PersonaDAO:
+    """
+    DAO significa: Data Access Object
+    CRUD significa:
+                  Create -> Insertar
+                  Read -> Seleccionar
+                  Update -> Actualizar
+                  Delete -> Eliminar
+    
+    _SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
+    _INSERTAR = 'INSERT INTO persona(nombre, apellido, email) VALUES(%s, %s, %s)'
+    _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
+    """
+
+    # Definimos los metodos de la clase
+    @classmethod
+    def seleccionar(clase):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                cursor.execute(clase._SELECCIONAR)
+                registros = cursor.fetchall()
+                personas = []  # Creamos una lista
+                for registro in registros:
+                    persona = Persona(registro[0], registro[1], registro[2], registro[3])
+                    personas.append(persona)
+                return personas
+
+    @classmethod
+    def insertar(clase, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email)
+                cursor.execute(clase._INSERTAR, valores)
+                log.debug(f'Persona Insertada: {persona}')
+                return cursor.rowcount
+
+    @classmethod
+    def actualizar(clase, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerConexion() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email, persona.id_persona)
+                cursor.execute(clase._ACTUALIZAR, valores)
+                log.debug(f'Persona actualizada: {persona}')
+                return cursor.rowcount
+
+    @classmethod
+    def eliminar(clase, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.id_persona)
+                cursor.execute(clase._ELIMINAR, valores)
+                log.debug(f'Los objetos eliminados son: {persona}')
+                return cursor.rowcount
+
+#para continuar
