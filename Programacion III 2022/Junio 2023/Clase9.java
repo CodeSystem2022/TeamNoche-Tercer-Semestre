3ebfe58cfd0502abfc1dfdb5a09f6c11f50d7e2d
Flor Pons Toloza <111473222+FlorPons@users.noreply.github.com>
2023-06-27 20:55:09 -0300

    Update Clase9.java
    Creamos CalculadoraUTN
    Definimos las variables

--- Programacion III 2022/Junio 2023/Clase9.java
+++ Programacion III 2022/Junio 2023/Clase9.java
@@ -1,1 +1,14 @@
+//Florencia Pons
+//Creamos CalculadoraUTN
+//Definimos las variables
+public class CalculadoraUTN {
+    public static void main(String[] args){
+        System.out.println("****** Aplicaci[on Calculadora ******");
+        var operando1 = 10;
+        var operando2 = 20;
+        var resultado = operando1 + operando2;
+        System.out.println("Resultado = " + resultado);
+    }
+
+}
 
