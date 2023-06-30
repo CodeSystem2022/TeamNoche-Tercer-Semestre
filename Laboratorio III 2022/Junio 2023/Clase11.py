89fc381acf22b97b841c15c2358e813b58a5d17a
Yamila Contreras <112906065+Yami-Contreras@users.noreply.github.com>
2023-06-30 17:39:20 -0300

    Update Clase11.py

--- Laboratorio III 2022/Junio 2023/Clase11.py
+++ Laboratorio III 2022/Junio 2023/Clase11.py
@@ -6,3 +6,22 @@ def liberarConexion(cls, conexion):
     cls.obtenerPool().putconn(conexion)
     logging.debug(f'Regresamos la conexion del pool: {conexion}')
   ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Alumna Yamila Contreras
+
+# Método cerrarConexiones()
+
+@classmethod
+def cerrarConexxiones(cls):
+    cls.obtenerPool().closeall()
+
+if __name__ =='__main__':
+    conexion1 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion1)
+    conexion2 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion2)
+    conexion3 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion3)
+    conexion4 = Conexion.obtenerConexion()
+    conexion5 = Conexion.obtenerConexion()
+    conexion6 = Conexion.obtenerConexion()
+    -------------------------------------------------------------------------------------------------------------------------------------------------------------------
