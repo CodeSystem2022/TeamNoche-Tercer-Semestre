137dbb900a57c7dbd34a2e00f30379ba230e1ba9
Yamila Contreras <112906065+Yami-Contreras@users.noreply.github.com>
2023-06-30 17:58:00 -0300

    Update Clase11.py

--- Laboratorio III 2022/Junio 2023/Clase11.py
+++ Laboratorio III 2022/Junio 2023/Clase11.py
@@ -1,5 +1,5 @@
 # Alumna Yamila Contreras
-# Método liberarConexion()
+# 1.1 Método liberarConexion()
 
 @classmethod
 def liberarConexion(cls, conexion):
@@ -8,7 +8,7 @@ def liberarConexion(cls, conexion):
   ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Alumna Yamila Contreras
 
-# Método cerrarConexiones()
+# 1.2 Método cerrarConexiones()
 
 @classmethod
 def cerrarConexxiones(cls):
@@ -25,3 +25,22 @@ if __name__ =='__main__':
     conexion5 = Conexion.obtenerConexion()
     conexion6 = Conexion.obtenerConexion()
     -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Alumna Yamila Contreras
+
+#1.3 Creación de la Clase CursorDelPool - Parte 1
+
+from logger_base import log
+from  conexion import Conexion
+
+
+class CursorDelPool:
+    def __init__(self):
+        self._conexion = None
+        self._cursor = None
+
+    def __enter__(self):
+        log.debug('Inicio del método with y __enter__')
+        self._conexion = Conexion.obtenerConexion()
+        self._cursor = self._conexion.cursor()
+        return self._cursor
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
