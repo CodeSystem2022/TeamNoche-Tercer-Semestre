e8411a18b3d22ade0769631fa31875d1f0277da0
Bruno Marchetti <107437629+Br1marchetti@users.noreply.github.com>
2023-07-04 16:55:47 -0300

    Update Clase9.java

--- Programacion III 2022/Junio 2023/Clase9.java
+++ Programacion III 2022/Junio 2023/Clase9.java
@@ -26,3 +26,19 @@ public class CalculadoraUTN {
                 if(operacion >= 1 && operacion <= 4){
                     ejecutarOperacion(operacion, entrada);
             } //Fin del if
+else if (operacion == 5) {
+                System.out.println("Hasta pronto...");
+                break; //Rompe el ciclo y sale
+            }
+            else {
+                System.out.println("Opcion erronea: "+operacion);
+            }
+            // Imprimimos un salto de linea antes de repetir el menu
+            System.out.println();
+
+            } catch (Exception e){ //Fin del try, comienzo del catch
+                System.out.println("Ocurrio ubn error: "+e.getMessage());
+                System.out.println();
+            } //Fin catch
+       } //Fin while
+    } //Fin main
