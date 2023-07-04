ade3f0720e3fce6c48b40e1a89cc6260b972d4c2
Bruno Marchetti <107437629+Br1marchetti@users.noreply.github.com>
2023-07-04 16:55:17 -0300

    Creacion calculadora y ciclo while.java

--- Programacion III 2022/Junio 2023/Clase9.java
+++ Programacion III 2022/Junio 2023/Clase9.java
@@ -11,4 +11,18 @@ public class CalculadoraUTN {
     }
 
 }
+//BRUNO MARCHETTI
+import java.util.Scanner;
 
+public class CalculadoraUTN {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        while (true) { //Ciclo while
+            System.out.println("******* Aplicacion Calculadora *******");
+            mostarMenu();
+
+            try {
+                var operacion = Integer.parseInt(entrada.nextLine());
+                if(operacion >= 1 && operacion <= 4){
+                    ejecutarOperacion(operacion, entrada);
+            } //Fin del if
