b49b29e2247666dbac12eaaf234b4451325826e5
Carlos Hernan <86528810+Hernan-DOS@users.noreply.github.com>
2023-07-05 00:20:01 -0300

    Update clase 9.js

--- Laboratorio III 2022/Junio 2023/clase 9.js
+++ Laboratorio III 2022/Junio 2023/clase 9.js
@@ -1,1 +1,6 @@
+9.1 Introducción a lo que es la herencia en JavaScript
+Carlos Hernan Suarez
 
+Explicación UML de Herencia - en la clase persona se define atributos y objetos  En clases hijas cliente y empleado 
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
