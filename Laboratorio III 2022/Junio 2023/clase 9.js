f238cbce64c9ab6dca4ae31f2d823acd357efc7f
Carlos Hernan <86528810+Hernan-DOS@users.noreply.github.com>
2023-07-05 00:49:23 -0300

    Update clase 9.js
    cracion clase persona

--- Laboratorio III 2022/Junio 2023/clase 9.js
+++ Laboratorio III 2022/Junio 2023/clase 9.js
@@ -4,4 +4,54 @@
 Explicación UML de Herencia - en la clase persona se define atributos y objetos  En clases hijas cliente y empleado 
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//9.2 Creacion de clase Persona
 
+
+class Persona {
+    static contadorPersonas = 0;
+
+    constructor(nombre, apellido, edad) {
+        this._idPersona = ++Persona.contadorPersonas;
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._edad = edad;
+    }
+
+    get _idPersona() {
+        return this._idPersona;
+    }
+
+    get nombre() {
+        return this._nombre;
+    }
+
+    set nombre(Nombre) {
+        this._nombre = Nombre;
+    }
+
+    get apellido() {
+        return this._apellido;
+    }
+
+    set apellido(Apellido) {
+        this._apellido = Apellido;
+    }
+
+    get edad() {
+        return this._edad;
+    }
+
+    set edad(Edad) {
+        this._edad = Edad;
+    }
+
+    toString(){
+        return `${this._idPersona} 
+        ${this._nombre} 
+        ${this._apellido} 
+        ${this._edad}`;
+    }
+
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//
