d705c9219da13a762244d348e7296905d724975e
Yamila Contreras <112906065+Yami-Contreras@users.noreply.github.com>
2023-07-04 22:17:57 -0300

    Update Clase11.py

--- Laboratorio III 2022/Junio 2023/Clase11.py
+++ Laboratorio III 2022/Junio 2023/Clase11.py
@@ -120,6 +120,7 @@ if __name__ == '__main__':
     # Actualizar un registro
     # persona1 = Persona(1, 'Juan José', 'Pena', 'jjpena@mail.com')
     # personas_actualizadas = PersonaDAO.actualizar(persona1)
+    # log.debug(f'Personas actualizadas: {personas_actualizadas}')
 
     #Insetar un registro
     persona1 = Persona(nombre='Marcos', apellido='Tejada', email=tejadam@mail.com)
@@ -135,3 +136,24 @@ if __name__ == '__main__':
 # Alumna: Yamila Belen Contreras
 
 # Pruebas de personaDAO - Parte 2
+
+if __name__ == '__main__':
+    # Eliminar un registro
+    persona1 = Persona(id_persona=18)
+    personas_eliminadas = PersonaDAO.eliminar(persona1)
+    log.debug(f'Personas eliminadas: {personas_eliminadas}')
+
+    # Actualizar un registro
+    persona1 = Persona(1, 'Juan', 'Pena', 'jpena@mail.com')
+    personas_actualizadas = PersonaDAO.actualizar(persona1)
+    log.debug(f'Personas actualizadas: {personas_actualizadas}')
+
+    #Insetar un registro
+    persona1 = Persona(nombre='Marcos', apellido='Torres', email=tmateo@mail.com)
+    personas_insertadas = PersonaDAO.insetar(persona1)
+    log.debug(f'Personas insertadas: {personas_insertadas}')
+
+    # Seleccionar objetos
+    personas = PersonaDao.seleccionar()
+    for persona in personas
+        log.debug(persona)
