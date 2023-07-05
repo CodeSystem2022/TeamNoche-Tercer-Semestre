54c1234539c7e5491f965d88649afa0fa0f488e7
Analia "Luna" Alvarenga <101668956+RastaLunaRL@users.noreply.github.com>
2023-07-05 02:02:39 -0300

    Clase6 java: Instalacion docker

--- Programacion III 2022/Mayo 2023/Clase6.java
+++ Programacion III 2022/Mayo 2023/Clase6.java
@@ -1,8 +1,4 @@
 //Analia Alvarenga (Luna)
 //DOCKER
 
-[DOCKER](https://campus.frsr.utn.edu.ar/moodle/mod/lesson/view.php?id=34067)
-
-//
-
-![GUIA DE INSTALACION DE DOCKER](https://geekflare.com/es/docker-installation-guide/)
+![Instalacion Docker](https://courseit.io/static/docker.pdf)
